30cbd5e9b4977ca4b3a380445ff6697adab41e18
brunodev11 <bweur123@gmail.com>
2021-02-16 08:16:00 -0800

    - finish

--- src/screens/Dashboard/MyAccount/ChangePassword/styles.js
+++ src/screens/Dashboard/MyAccount/ChangePassword/styles.js
@@ -2,19 +2,29 @@ import {StyleSheet} from 'react-native';
 import {Colors, Fonts} from 'constant';
 
 export default StyleSheet.create({
-  spaceBox: {
-    marginTop: 10,
+  container: {
+    flex: 1,
+    backgroundColor: Colors.bg,
   },
-  warning: {
-    paddingHorizontal: 15,
-    paddingVertical: 20,
-    flexDirection: 'row',
-    backgroundColor: Colors.white,
+  innerContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
-  warnText: {
-    fontSize: 13,
+  heading: {
+    fontSize: 15,
     color: Colors.header_title,
+    textAlign: 'center',
     fontFamily: Fonts.AvenirNextRegular,
-    marginLeft: 8,
+    backgroundColor: Colors.white,
+    padding: 20,
+  },
+  emailText: {
+    marginTop: 30,
+    fontSize: 18,
+    color: Colors.header_title,
+    textAlign: 'center',
+    fontWeight: '500',
+    fontFamily: Fonts.AvenirNextRegular,
   },
 });
